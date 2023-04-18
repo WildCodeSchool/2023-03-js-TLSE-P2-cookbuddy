f475e7658bb49112e6fb916f99c79033f7b0e68f
JIMI_BES <jimi.bes@gmail.com>
2023-04-18 17:26:10 +0200

    api fetched and test done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
 import "./styles/reset.css";
 import "./styles/App.scss";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [recipeData, setRecipeData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getRecipeData = () => {
+      axios
+        .get(
+          `https://api.edamam.com/api/recipes/v2?type=public&q="bread"&app_id=${
+            import.meta.env.VITE_APP_ID_CF
+          }&app_key=${import.meta.env.VITE_APP_KEY_CF}`
+        )
+        .then((response) => {
+          setRecipeData(response.data.hits);
+          setIsLoading(true);
+        });
+    };
+    getRecipeData();
+  }, []);
+
   return (
-    <div className="App">
+    <div>
       <p>CookBuddy</p>
+      {isLoading ? <p>{recipeData[0].recipe.label}</p> : <p>Loading...</p>}
     </div>
   );
 }
