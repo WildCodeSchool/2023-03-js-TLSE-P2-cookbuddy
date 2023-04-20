bb59926d65fcaaeb67269d54c1197312efe2517b
JIMI_BES <jimi.bes@gmail.com>
2023-04-20 17:31:00 +0200

    responsive style started

--- frontend/src/components/RecipeDetails.jsx
+++ frontend/src/components/RecipeDetails.jsx
@@ -9,75 +9,77 @@ function RecipeDetails({ data }) {
     return Number.isInteger(num) ? num.toString() : num.toFixed(2);
   }
   return (
-    <div className="recipe-details">
-      <button className="close" type="button">
-        <i className="bi bi-x-lg" />
-      </button>
-      <div className="thumbnail">
-        <img src={data.image} alt="Recipe illustration" />
-      </div>
-      <div className="recipe-description">
-        <h2 className="recipe-label">{data.label}</h2>
-        <div className="recipe-infos">
-          <p>
-            <i className="bi bi-fire" />{" "}
-            {numberRounded(data.totalNutrients.ENERC_KCAL.quantity)}{" "}
-            {data.totalNutrients.ENERC_KCAL.unit}
-          </p>
-          <p>
-            <i className="bi bi-bookmarks-fill" /> {data.mealType}
-          </p>
-          <p>
-            <i className="bi bi-clock-history" /> {data.totalTime} minutes
-          </p>
-          <p>
-            <i className="bi bi-globe-americas" /> {data.cuisineType}
-          </p>
+    <div className="recipe-container">
+      <div className="recipe-details">
+        <button className="close" type="button">
+          <i className="bi bi-x-lg" />
+        </button>
+        <div className="thumbnail">
+          <img src={data.image} alt="Recipe illustration" />
         </div>
-        <div className="recipe-nutrients">
-          <p>
-            {data.totalNutrients.FAT.label}{" "}
-            {numberRounded(data.totalNutrients.FAT.quantity)}
-            {data.totalNutrients.PROCNT.unit}
-          </p>
-          <p>
-            {data.totalNutrients.PROCNT.label}{" "}
-            {numberRounded(data.totalNutrients.PROCNT.quantity)}
-            {data.totalNutrients.PROCNT.unit}
-          </p>
-          <p>
-            {data.totalNutrients.CHOCDF.label}{" "}
-            {numberRounded(data.totalNutrients.CHOCDF.quantity)}
-            {data.totalNutrients.PROCNT.unit}
-          </p>
-          <p>
-            {data.totalNutrients.FIBTG.label}{" "}
-            {numberRounded(data.totalNutrients.FIBTG.quantity)}
-            {data.totalNutrients.PROCNT.unit}
-          </p>
+        <div className="recipe-description">
+          <h2 className="recipe-label">{data.label}</h2>
+          <div className="recipe-infos">
+            <p>
+              <i className="bi bi-fire" />{" "}
+              {numberRounded(data.totalNutrients.ENERC_KCAL.quantity)}{" "}
+              {data.totalNutrients.ENERC_KCAL.unit}
+            </p>
+            <p>
+              <i className="bi bi-bookmarks-fill" /> {data.mealType}
+            </p>
+            <p>
+              <i className="bi bi-clock-history" /> {data.totalTime} minutes
+            </p>
+            <p>
+              <i className="bi bi-globe-americas" /> {data.cuisineType}
+            </p>
+          </div>
+          <div className="recipe-nutrients">
+            <p>
+              {data.totalNutrients.FAT.label}{" "}
+              {numberRounded(data.totalNutrients.FAT.quantity)}
+              {data.totalNutrients.PROCNT.unit}
+            </p>
+            <p>
+              {data.totalNutrients.PROCNT.label}{" "}
+              {numberRounded(data.totalNutrients.PROCNT.quantity)}
+              {data.totalNutrients.PROCNT.unit}
+            </p>
+            <p>
+              {data.totalNutrients.CHOCDF.label}{" "}
+              {numberRounded(data.totalNutrients.CHOCDF.quantity)}
+              {data.totalNutrients.PROCNT.unit}
+            </p>
+            <p>
+              {data.totalNutrients.FIBTG.label}{" "}
+              {numberRounded(data.totalNutrients.FIBTG.quantity)}
+              {data.totalNutrients.PROCNT.unit}
+            </p>
+          </div>
+          <h2 className="ingredientsTitle">Ingredients</h2>
+          <ul>
+            {data.ingredients.map((ingredient) => (
+              <li key={ingredient.id}>
+                <label htmlFor={ingredient.id}>
+                  <input
+                    className="checkmark"
+                    type="checkbox"
+                    id={ingredient.id}
+                    name={ingredient.food}
+                  />
+                  {truncateNumber(ingredient.quantity)}{" "}
+                  {ingredient.measure !== "<unit>" ? ingredient.measure : ""}{" "}
+                  {ingredient.food}
+                </label>
+              </li>
+            ))}
+          </ul>
         </div>
-        <h2 className="ingredientsTitle">Ingredients</h2>
-        <ul>
-          {data.ingredients.map((ingredient) => (
-            <li key={ingredient.id}>
-              <label htmlFor={ingredient.id}>
-                <input
-                  className="checkmark"
-                  type="checkbox"
-                  id={ingredient.id}
-                  name={ingredient.food}
-                />
-                {truncateNumber(ingredient.quantity)}{" "}
-                {ingredient.measure !== "<unit>" ? ingredient.measure : ""}{" "}
-                {ingredient.food}
-              </label>
-            </li>
-          ))}
-        </ul>
+        <button className="action-button--xl action-button--full" type="button">
+          Discover the instructions <i className="bi bi-box-arrow-up-right" />
+        </button>
       </div>
-      <button className="action-button--xl action-button--full" type="button">
-        Discover the instructions <i className="bi bi-box-arrow-up-right" />
-      </button>
     </div>
   );
 }
