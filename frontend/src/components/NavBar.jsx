4b29927fa956a614a20ed18188c5fbfe2924f83e
AnneClr <108940832+AnneClr@users.noreply.github.com>
2023-04-25 11:37:23 +0200

    button filter deleted

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,17 +1,13 @@
 import "../styles/App.scss";
 import "../styles/components/NavBar.scss";
+import logo from "../assets/logo-color-full.svg";
 
 export default function NavBar() {
   return (
     <>
       <div className="header-container">
         <div className="navbar">
-          <img
-            className="logo"
-            src="./src/assets/logo-color-full.svg"
-            alt="logo Cook Buddy"
-          />
-
+          <img className="logo" src={logo} alt="logo Cook Buddy" />
           <button className="dark-mode" type="button">
             <i className="bi bi-moon-fill" />
           </button>
@@ -23,9 +19,6 @@ export default function NavBar() {
           type="text"
           placeholder="Enter ingredients or recipe"
         />
-        <button className="filter" type="button">
-          <i className="bi bi-sliders" />
-        </button>
       </div>
     </>
   );
