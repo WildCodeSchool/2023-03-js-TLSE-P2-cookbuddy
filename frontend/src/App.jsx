d066b6751fe534d3798b42f5fa0539b3c45c9929
JulienC90 <napalm81370@gmail.com>
2023-04-26 10:25:09 +0200

    S3_F01_US6_category-button-link: added link to 'CategoryButton.jsx' button and added matching key/value in 'utils.jsx' + showing search parameters inside URL after clicking a dish type button and landing on results page 'Search.jsx'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
+        <Route path="/search/*" element={<Search />} />
       </Routes>
     </Router>
   );
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,25 +1,51 @@
 import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Footer from "../components/Footer";
+import RecipesList from "../components/RecipesList";
 
 export default function Search() {
-  const [recipeData, setRecipeData] = useState();
+  const [recipesData, setRecipesData] = useState();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const apiURLtable = [
+    "https://api.edamam.com/api/recipes/v2?type=public&imageSize=LARGE&random=true",
+  ];
+  apiURLtable.push(
+    searchParams.get("q") ? `q=${searchParams.get("q")} ` : null
+  );
+  apiURLtable.push(`app_id=${import.meta.env.VITE_APP_ID_CF}`);
+  apiURLtable.push(`app_key=${import.meta.env.VITE_APP_KEY_CF}`);
+  apiURLtable.push(
+    searchParams.get("health") ? `health=${searchParams.get("health")} ` : null
+  );
+  apiURLtable.push(
+    searchParams.get("cuisineType")
+      ? `cuisineType=${searchParams.get("cuisineType")}`
+      : null
+  );
+  apiURLtable.push(
+    searchParams.get("mealType")
+      ? `mealType=${searchParams.get("mealType")}`
+      : null
+  );
+  apiURLtable.push(
+    searchParams.get("time")
+      ? ` time=${searchParams.get("time")} `
+      : "time=1%2B"
+  );
+
+  const apiURL = apiURLtable.filter((x) => x).join("&");
 
   useEffect(() => {
-    const getRecipeData = () => {
-      axios
-        .get(
-          `https://api.edamam.com/api/recipes/v2?type=public&q="bread"&app_id=${
-            import.meta.env.VITE_APP_ID_JC
-          }&app_key=${import.meta.env.VITE_APP_KEY_JC}`
-        )
-        .then((response) => {
-          setRecipeData(response.data.hits);
-          setIsLoaded(true);
-        });
+    const getRecipesData = () => {
+      axios.get(apiURL).then((response) => {
+        setRecipesData(response.data.hits);
+        setSearchParams(searchParams);
+        setIsLoaded(true);
+      });
     };
-    getRecipeData();
+    getRecipesData();
   }, []);
 
   return (
@@ -29,7 +55,11 @@ export default function Search() {
       </nav>
       <main>
         <div className="container">
-          {isLoaded ? <p>{recipeData[0].recipe.label}</p> : <p>Loading...</p>}
+          {isLoaded ? (
+            <RecipesList data={recipesData} listClass="home" />
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
       </main>
       <Footer />
